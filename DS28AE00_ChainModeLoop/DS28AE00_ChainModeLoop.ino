678c471840f3d8b28062d8b51166371c9a35d2c4
Cameron Planck <cameronplanck@gmail.com>
2022-11-15 11:04:47 -0500

    second commit with initial ds28ea00 looping arduino code

---
+++ DS28AE00_ChainModeLoop/DS28AE00_ChainModeLoop.ino
@@ -0,0 +1,112 @@
+// This code performs a discovery routine for N DS28AE00 chips chained together
+// it performs the discovery loop, records all the addresses in order, and then loop over the addresses to collect the temperatures
+// IT WORKS! IT ACTUALLY WORKS! LET'S SPIN SOME REAL BOARDS!
+// written 14 July 2022 by Cameron Planck
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire  ds(12);  // on pin 12 (a 4.7K resistor is necessary)
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&ds);
+
+void setup(void) {
+   // Start serial communication for debugging purposes
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+  
+}
+
+// array carrying device addresses. It should be size 8*N_devices
+byte ROMarray[40];
+byte present = 0;
+
+
+void chipDiscover(int k){
+
+     // this function applies the chain command and control sequence, 
+
+    Serial.print("The value of K is: ");
+    Serial.println(k);
+
+    byte addr[8];
+    byte i;
+    
+    Serial.println("------------------ CHIP DISCOVER BEGIN --------------------");
+  
+    // reset 1-wire bus
+    present = ds.reset();
+    Serial.print("Reseting again. Chip still present?: ");
+    Serial.println(present);
+  
+    // execute conditional read ROM command and print response (64-bit registration number)
+    Serial.println("Applying Conditional Read ROM Command...");
+    ds.write(0x0F);
+    Serial.print("Response: ");
+    for (i = 0; i < 8; i++) {          
+      addr[i] = ds.read();
+      Serial.print(addr[i], HEX);
+      Serial.print(" ");
+      ROMarray[k*8 + i] = addr[i];
+    }
+    
+//    delay(1000);
+  
+    // reset i-wire bus
+    present = ds.reset();
+    Serial.println();
+    Serial.print("Reseting again. Chip still present?: ");
+    Serial.println(present);
+  
+    // execute match ROM to select conditional chip
+    Serial.println("Applying Match ROM Command...");
+    ds.write(0x55);
+    for (i = 0; i < 8; i++) {          
+      ds.write(addr[i]);
+      Serial.print(addr[i], HEX);
+    }
+    Serial.println("");
+  
+    Serial.print("Applying Chain Done command. Result: ");
+    Serial.println(sensors.chainDone(), HEX);
+    
+  }
+
+
+
+
+void loop(void){ 
+
+  // device count from Dallas Temperature library
+  // note this number might include other 1-wire chips on board in addition to the DS28AE00s
+  // a better way to end the discovery processs is to run the discovery loop until <8 Bytes FFh>
+//  Serial.print("Number of 1-wire chips discovered on bus: ");
+//  Serial.println(sensors.getDeviceCount()); 
+
+  present = ds.reset();
+  Serial.print("Present?: ");
+  Serial.println(present);
+
+  // turn on chain mode
+  Serial.print("Turning on chain mode. Result: ");
+  Serial.println(sensors.chainOn(), HEX);
+
+  // loop over chips, applying Conditional ROM command and writing response to ROMarray
+  for(int i=0; i<5;i++){
+    chipDiscover(i);
+  }
+
+  // fetch temperatures from each thermistor by indexing ROMarray
+  sensors.requestTemperatures();
+  for(int i = 0; i<5; i++){
+    DeviceAddress thermistor = {ROMarray[i*8], ROMarray[i*8 +1], ROMarray[i*8 +2], ROMarray[i*8 +3], ROMarray[i*8 +4], ROMarray[i*8 +5], ROMarray[i*8 +6], ROMarray[i*8 +7]};
+//    sensors.requestTemperaturesByAddress(thermistor);
+    Serial.print("THERMISTOR ");
+    Serial.print(i+1);
+    Serial.print(" READING: ");
+    Serial.println(sensors.getTempC(thermistor));
+  }
+  
+}
