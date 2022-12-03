fd1d5f4830385405f7e52e66520a405a8d630413
Cameron Planck <cameronplanck@gmail.com>
2022-12-02 22:58:55 -0500

    bug improvements

--- ow-tempstring-main/tempstring-controller/tempstring-controller.ino
+++ ow-tempstring-main/tempstring-controller/tempstring-controller.ino
@@ -1,28 +1,75 @@
 // this is test code for the one-wire control Arduino. It recieves commands from the SIMB3 main board. 
 
-#include <Wire.h>                            
-byte I2C_OnOff;
+// modify this code to send data back to master 
+
+#include <Wire.h>       
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire  ds(12);  // on pin 12 (a 4.7K (ish) resistor is necessary, it wont work without it)
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&ds);
+                     
+byte command;
+#define ANSWERSIZE 50
+String answer = "Hello";
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   Wire.begin(1);                                
-  Wire.onReceive(BlinkLED);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  sensors.begin();
 }
 
 void loop() {
 delay(100);
 }
 
-void BlinkLED(int Press)
-{
-  I2C_OnOff = Wire.read();                      
-  if (I2C_OnOff == 1)
-  {
+
+// called when master sends data
+void receiveEvent(int numBytes){
+  
+  command = Wire.read();
+
+  if (command == 1){
    digitalWrite(LED_BUILTIN, HIGH);                 
   }
-  else if (I2C_OnOff == 0)
-  {
+  
+  else if (command == 0){
    digitalWrite(LED_BUILTIN, LOW);                 
   } 
+
+  else if (command == 2){
+    answer = "Hi";
+  }
+  else if (command == 3){
+    answer = "it me";
+  }
+
+  else if (command == 0x55){
+    answer = "general string";
+  }
+}
+
+// called when master asks for data
+void requestEvent() {
+ 
+  // Setup byte variable in the correct size
+  byte response[ANSWERSIZE];
+  
+  // Format answer as array
+  for (byte i=0;i<ANSWERSIZE;i++) {
+    response[i] = (byte)answer.charAt(i);
+  }
+
+  sensors.requestTemperatures();
+  int temp = sensors.getTempCByIndex(0)*100;
+
+    Wire.write(temp,sizeof(temp));
+
+  
+  // Send response back to Master
+//  Wire.write(response,sizeof(response));
 }
--- ow-tempstring-main/tempstring-mainboard/tempstring-mainboard.ino
+++ ow-tempstring-main/tempstring-mainboard/tempstring-mainboard.ino
@@ -1,4 +1,5 @@
 // this is test code for the SIMB3 main board to talk to the one-wire control Arduino over i2c. 
+// it is used in conjunction with the temperaturestring-controller.ino sketch for the controller. 
 
 // hardware setup includes an SIMB3 mainboard with a feather m0 plugged in through the aux i2c line. 
 // note that battery power (17v) must be applied for the system to work. 
@@ -7,9 +8,13 @@
 // Control the LED on the tempstring-meatball by writing 'On' and 'Off' commands
 
 
+// General flow:
+// For sending data to peripheral, simply run a Wire.beginTransmission, Wire.write(your data), and Wire.endTransmission. 
+// On the peripheral side, this triggers an onRecieve() event which calls a function that can unpack the data sent from the master
+
 #include <Wire.h>                                                        
 String readString;                                                          
-byte I2C_OnOff;   
+byte command;   
 
 #define NUBC_5V_ENABLE          5
 
@@ -28,20 +33,43 @@ void loop() {
 
   if (readString == "On")
   {
-    I2C_OnOff = 1;
+    command = 1;
     Serial.print("TURNING LED ON...");
   }
   else if (readString == "Off")
   {
-    I2C_OnOff = 0;
+    command = 0;
     Serial.print("TURNING LED OFF...");
 
   }
+  else if (readString == "Hi"){
+    command = 2;
+  }
+
+  else if (readString == "it me"){
+    command = 3;
+  }
+
+  else if (readString == "Hex"){
+    command = 0x55;
+  }
+
+    else if (readString == "Fetch")
+  {
+    Serial.print("Fetching data...");
+    Wire.requestFrom(1,50);
+    while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+    }
+  }
+
+  
   if (readString.length() > 0)                                            
   {                                
     readString = "";                                                        
   }
   Wire.beginTransmission(1);                                                
-  Wire.write(I2C_OnOff);                                                    
+  Wire.write(command);                                                    
   Wire.endTransmission();    
 }
