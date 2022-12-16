fb5ff9ea98d92a30727db643f86b147c7ad76249
Cameron Planck <cameronplanck@gmail.com>
2022-12-16 18:31:09 -0500

    commit before switching gears to WRT APEX project

--- Development/Arduino/Mainboard + Control Board Sketches/Fetch Data Over I2C/fetch-data-slave-ROM-store/fetch-data-slave-ROM-store.ino
+++ Development/Arduino/Mainboard + Control Board Sketches/Fetch Data Over I2C/fetch-data-slave-ROM-store/fetch-data-slave-ROM-store.ino
@@ -13,23 +13,42 @@
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Adafruit_NeoPixel.h>
 #include <Adafruit_Express_Flash.h>
 
-OneWire  ds(12);  // on pin 12 (a 4.7K (ish) resistor is necessary, it wont work without it)
+// bottom string one-wire instance
+OneWire  bs(11);  // on pin 12 (a 4.7K (ish) resistor is necessary, it wont work without it)
+
+// top string one-wire instance
+OneWire  ts(12);  // on pin 12 (a 4.7K (ish) resistor is necessary, it wont work without it)
+
+// air-temp one-wire instance
 OneWire  at(13);  //  on pin 13 (a 4.7K (ish) resistor is necessary, it wont work without it)
 
-// Pass our oneWire reference to Dallas Temperature sensor
-DallasTemperature sensors(&ds);
+// Pass our oneWire reference to Dallas Temperature library
+DallasTemperature top_string(&ts);
+DallasTemperature bottom_string(&bs);
 DallasTemperature air_temp(&at);
 
 Adafruit_Express_Flash onBoardFlash;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 8, NEO_GRB + NEO_KHZ800);
 
+
+#define ROM_SEARCH_ENABLE 5
 
 void setup() {
   Serial.begin(9600);
 
+  // set ROM search pin (pin 5)
+  pinMode(ROM_SEARCH_ENABLE, INPUT); 
+
   // intitialize flash storage
   onBoardFlash.initialize();
+
+  // enable pixel RBG
+  pixels.begin();
+  pixels.setBrightness(0);
+
 
   // initialize I2C with address 1
   Wire.begin(1);
@@ -40,9 +59,11 @@ void setup() {
   // runs when master asks for data
   Wire.onRequest(requestEvent);
 
-  // start one-wire devices
-  sensors.begin();
+  // start up one-wire devices
+  top_string.begin();
+  bottom_string.begin();
   air_temp.begin();
+ 
 
 }
 
@@ -72,8 +93,8 @@ byte tempPacket[32];
 // called when master sends data
 void receiveEvent(int numBytes) {
   while (Wire.available()) {
-    state = Wire.read();
-    packetIndex = Wire.read();
+    state = Wire.read(); // first byte in "command"
+    packetIndex = Wire.read(); // second byte in "command"
   }
 }
 
@@ -91,7 +112,20 @@ void requestEvent() {
     Wire.write(tempPacket, 32);
     packetIndex++;
   }
+
+  else if (state == 2 && packetIndex == 0) {
+    readBottomString();
+    packTempPacket(packetIndex);
+    Wire.write(tempPacket, 32);
+    packetIndex++;
+  }
+
   else if (state == 2) {
+    packTempPacket(packetIndex);
+    Wire.write(tempPacket, 32);
+    packetIndex++;
+  }
+  else if (state == 3) {
     readAirTemp();
     Wire.write(airTempArray, 2);
   }
@@ -111,49 +145,131 @@ void readTopString() {
   Serial.println("===================== READING TOP TEMPERATURE STRING ===================== ");
   Serial.println("Initialize One-Wire....");
   Serial.print("Number of one-wire devices detected: ");
-  Serial.println(sensors.getDeviceCount());
+  Serial.println(top_string.getDeviceCount());
 
   // ask all chips to perform temperature conversion
-  sensors.requestTemperatures();
+  top_string.requestTemperatures();
 
-  present = ds.reset();
+  present = ts.reset();
   Serial.print("Present?: ");
   Serial.println(present);
 
   // turn chain mode on
-  sensors.chainOn();
+  top_string.chainOn();
 
   // run chip discover process to generate ROMarray
   // if pin 2 is pulled high, execute the chip discovery process and store to onboard Flash memory
-  if (digitalRead(2) == HIGH) {
+  if (digitalRead(ROM_SEARCH_ENABLE) == HIGH) {
+    pixels.setBrightness(50);
+    pixels.setPixelColor(0, pixels.Color(121,0,255));
+    pixels.show();
+    
+    Serial.println("ROM SEARCH ENABLED");
     Serial.println("Beginning chip discovery...");
     for (int i = 0; i < chipNum; i++) {
       chipDiscover(i);
     };
-    onBoardFlash.writebytes("TOPSTRING_ROMS.csv", ROMarray);
+    onBoardFlash.format();
+    onBoardFlash.writebytes("TOPSTRING_ROMS.csv", ROMarray, 640);
+    pixels.setBrightness(0);
+    pixels.show();
   }
   
-  // if pin 2 is NOT pulled high, retrieve ROMs from onboard Flash memory
+  // if pin 5 is NOT pulled high, retrieve ROMs from onboard Flash memory
   else {
+    pixels.setBrightness(50);
+    pixels.setPixelColor(0, pixels.Color(0,255,14));
+    pixels.show();
+    Serial.println("ROM SEARCH DISABLED");
     Serial.println("Getting temperature bytes...");
-    onBoardFlash.readbytes("TOPSTRING_ROMS.csv", ROMarray);
+    onBoardFlash.readbytes("TOPSTRING_ROMS.csv", ROMarray, 640);
     for (int i = 0; i < chipNum; i++) {
-      ds.reset();
+      ts.reset();
       chipSelect(i);
     }
+    pixels.setBrightness(0);
+    pixels.show();
   }
 
 
   // loop over ROMarray, select each chip, ask for temperature, and write to tempArray
   Serial.println("Getting temperature bytes...");
   for (int i = 0; i < chipNum; i++) {
-    ds.reset();
+    ts.reset();
     chipSelect(i);
   }
 
   printTemperatureString();
 
 }
+
+
+void readBottomString() {
+
+  Serial.println("===================== READING BOTTOM TEMPERATURE STRING ===================== ");
+  Serial.println("Initialize One-Wire....");
+  Serial.print("Number of one-wire devices detected: ");
+  Serial.println(bottom_string.getDeviceCount());
+
+  // ask all chips to perform temperature conversion
+  bottom_string.requestTemperatures();
+
+  present = bs.reset();
+  Serial.print("Present?: ");
+  Serial.println(present);
+
+  // turn chain mode on
+  bottom_string.chainOn();
+
+  // run chip discover process to generate ROMarray
+  // if pin 2 is pulled high, execute the chip discovery process and store to onboard Flash memory
+  if (digitalRead(ROM_SEARCH_ENABLE) == HIGH) {
+    pixels.setBrightness(50);
+    pixels.setPixelColor(0, pixels.Color(121,0,255));
+    pixels.show();
+    
+    Serial.println("ROM SEARCH ENABLED");
+    Serial.println("Beginning chip discovery...");
+    for (int i = 0; i < chipNum; i++) {
+      chipDiscover(i);
+    };
+    onBoardFlash.format();
+    onBoardFlash.writebytes("BOTTOMSTRING_ROMS.csv", ROMarray, 640);
+    pixels.setBrightness(0);
+    pixels.show();
+  }
+  
+  // if pin 5 is NOT pulled high, retrieve ROMs from onboard Flash memory
+  else {
+    pixels.setBrightness(50);
+    pixels.setPixelColor(0, pixels.Color(0,255,14));
+    pixels.show();
+    Serial.println("ROM SEARCH DISABLED");
+    Serial.println("Getting temperature bytes...");
+    onBoardFlash.readbytes("TOPSTRING_ROMS.csv", ROMarray, 640);
+    for (int i = 0; i < chipNum; i++) {
+      bs.reset();
+      chipSelect(i);
+    }
+    pixels.setBrightness(0);
+    pixels.show();
+  }
+
+
+  // loop over ROMarray, select each chip, ask for temperature, and write to tempArray
+  Serial.println("Getting temperature bytes...");
+  for (int i = 0; i < chipNum; i++) {
+    bs.reset();
+    chipSelect(i);
+  }
+
+  printTemperatureString();
+
+}
+
+
+
+
 
 void chipDiscover(int k) {
 
@@ -162,16 +278,14 @@ void chipDiscover(int k) {
   byte i;
 
   // reset 1-wire bus
-  present = ds.reset();
-  //    Serial.print("Reseting again. Chip still present?: ");
-  //    Serial.println(present);
+  present = ts.reset();
 
   // execute conditional read ROM command and print response (64-bit registration number)
   //    Serial.println("Applying Conditional Read ROM Command...");
-  ds.write(0x0F);
+  ts.write(0x0F);
   //    Serial.print("Chip Address: ");
   for (i = 0; i < 8; i++) {
-    addr[i] = ds.read();
+    addr[i] = ts.read();
     //      Serial.print(addr[i], HEX);
     //      Serial.print(" ");
     ROMarray[k * 8 + i] = addr[i];
@@ -180,7 +294,7 @@ void chipDiscover(int k) {
   //    Serial.println("");
 
   // turn chain mode off
-  sensors.chainDone();
+  top_string.chainDone();
 
 }
 
@@ -189,10 +303,10 @@ void chipSelect(int k) {
   // apply Match ROM command and select chip by writing ROM
   //    Serial.println("Selecting chip by ROM...");
   byte addr[8];
-  ds.write(0x55);
+  ts.write(0x55);
   for (int i = 0; i < 8; i++) {
     addr[i] = ROMarray[k * 8 + i];
-    ds.write(addr[i]);
+    ts.write(addr[i]);
     //      Serial.print(addr[i], HEX);
     //      Serial.print(" ");
   }
@@ -200,11 +314,11 @@ void chipSelect(int k) {
   //    Serial.println("");
 
   //    Serial.println("Reading Scratchpad...");
-  ds.write(0xBE); // read scratchpad command
+  ts.write(0xBE); // read scratchpad command
   byte scratchpad[2];
   //    Serial.print("Scratchpad Bytes: ");
   for (int i = 0; i < 2; i++) { // step through each byte, write it to ROMarray
-    scratchpad[i] = ds.read();
+    scratchpad[i] = ts.read();
     //      Serial.print(scratchpad[i], BIN);
     //      Serial.print(" ");
   }
@@ -219,6 +333,11 @@ void chipSelect(int k) {
 
 }
 
+
+
+
+
+
 void readAirTemp() {
 
   Serial.println("\n================= READING DS18B20 AIR TEMPERATURE SENSOR ================= ");
@@ -226,6 +345,9 @@ void readAirTemp() {
   air_temp.requestTemperatures();
   at.reset(); // reset one-wire bus
 
+  pixels.setBrightness(50);
+  pixels.setPixelColor(0, pixels.Color(0,255,234));
+  pixels.show();
   Serial.println("Getting temperature bytes...");
   at.write(0xCC); // skip ROM
   at.write(0xBE); // read scratchpad command
@@ -241,6 +363,8 @@ void readAirTemp() {
   }
 
   printAirTemperature();
+  pixels.setBrightness(0);
+  pixels.show();
 }
 
 void loop() {}
